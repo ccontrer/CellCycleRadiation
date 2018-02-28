593ae038d64595062848fa56b42a521880cb77ba
Carlos Contreras <carloscontrerasc@gmail.com>
2018-02-27 20:24:38 -0700

    CCRMV003 added. Parameters and figures updated.

--- Julia/custom_functions.jl
+++ Julia/custom_functions.jl
@@ -1,4 +1,4 @@
-using DataStructures
+using DataStructures, NamedTuples
 
 type BifurcationCurve{DType,SPType}
   d::DType
@@ -41,7 +41,7 @@ function bifurcation_curve_ccc(PC,name)  # Get the curve
           end
       end
   end
-  # stab = [sol[:labels][i][curve_point_type]["stab"] for i in 1:len]
+  #  stab = [sol[:labels][i][curve_point_type]["stab"] for i in 1:len]
   #else
   #  stab = []
   #end
@@ -74,4 +74,78 @@ function find_changes(stab)
     end
   end
   changes
+end
+
+function ntupdate(x::NamedTuple, y::Dict)
+    z = @NT()
+    for key in keys(x)
+        if key in keys(y)
+            z = NamedTuples.setindex(z, key, y[key])
+        else
+            z = NamedTuples.setindex(z, key, x[key])
+        end
+    end
+    z
+end
+               
+function find_limit_cycle(f, u0, tspan, params; alg=Rosenbrock23(), kwargs...)
+    function division(u,t,integrator)
+        ## Parameters
+        ## Set of condition: MPF crosses threshold from positive to negative
+        u[1] - params[31]#params.theta_M
+    end # ModelCond
+    function cycle_stop!(integrator)
+        terminate!(integrator)
+    end
+    cb_start = ContinuousCallback(division, nothing, cycle_stop!)
+    # Integrate until first division
+    # TODO: assert tspan is larger than needed
+    prob = ODEProblem(f, u0, tspan, params)
+    sol = solve(prob, alg, callback=cb_start; kwargs...)
+    # One full cycle
+    u0_start = sol.u[end]
+    prob = ODEProblem(f, u0_start, tspan, params)
+    sol = solve(prob, alg, callback=cb_start; kwargs...)
+    # Done
+    sol
+end #function find_cell_cycle
+
+function follow_limit_cycle(initcycle, pdomain, stepsize; kwargs...)
+    #p[32] should be p.Mass
+    cycle = deepcopy(initcycle)
+    f = cycle.prob.f
+    p = [cycle.prob.p[key] for key in keys(cycle.prob.p)]
+    # Start
+    minmax = extrema(cycle[1, :])
+    T = cycle.t[end] - cycle.t[1]
+    out = [[p[32], minmax[1], minmax[2], T]]
+    # Forward
+    while (p[32] < (pdomain[2] - stepsize))
+        #TODO: Fix this, use copy or something
+        p[32] += stepsize
+        u0 = cycle.u[end]
+        tspan = (0.0, 1.3*T)
+        cycle = find_limit_cycle(f, u0, tspan, p; kwargs...)
+        minmax = extrema(cycle[1, :])
+        T = cycle.t[end] - cycle.t[1]
+        push!(out, [p[32], minmax[1], minmax[2], T])
+    end
+    # Backward
+    cycle = deepcopy(initcycle)
+    f = cycle.prob.f
+    p = [cycle.prob.p[key] for key in keys(cycle.prob.p)]
+    while ((pdomain[1] + stepsize) < p[32])
+        p[32] -= stepsize
+        u0 = cycle.u[end]
+        tspan = (0.0, 1.8*T)
+        cycle = find_limit_cycle(f, u0, tspan, p; kwargs...)
+        minmax = extrema(cycle[1, :])
+        T = cycle.t[end] - cycle.t[1]
+        push!(out, [p[32], minmax[1], minmax[2], T])
+    end
+    temp = sort(out, by = x -> x[1])
+    temp = hcat(temp...)
+    dict1 = Dict([:min=>temp[2,:], :max=>temp[3,:], :length=>temp[4]])
+    dict2 = OrderedDict(Dict([:Mass=>temp[1,:], :MPF=>dict1]))
+    BifurcationCurve(dict2, repeat(["LC"], inner=[length(out)]), Dict{String,Any}(), Int[])
 end
--- Julia/plot_functions.jl
+++ Julia/plot_functions.jl
@@ -7,7 +7,7 @@ colG2 = "#698e6d" #green
 colM = "#a67ba7" #purple
 ## Colort pallete https://personal.sron.nl/~pault/
 blue1 = "#332288"
-blue2 = "#88ccee"
+blue2 = "#4467ff"
 blue3 = "#63bfeb"
 green1 = "#44aa99"
 green2 = "#117733"
@@ -21,8 +21,8 @@ gray1 = "#888888"
 
 @pyimport mpl_toolkits.axes_grid1 as axgrid
 
-function PlotSolution(sol, rad=false)
-    fig, ax1 = subplots()
+function PlotSolution(sol, rad=nothing)
+    fig, ax1 = subplots(figsize=(8.4, 4.8))
     mylw = 2
     ax2 = ax1[:twinx]()
     ax1[:plot](sol.t, sol[3, :], color=red1, lw=mylw, label="Wee1") # Wee1
@@ -31,41 +31,54 @@ function PlotSolution(sol, rad=false)
     # ax1[:plot](sol.t, sol[2, :], color = "LimeGreen", lw=mylw, label="MPF\$_P\$") # MPF_P
     ax1[:plot](sol.t, sol[1, :], color=green2, lw=mylw, label="MPF") # MPF
     ax2[:plot](sol.t, sol[7, :], color="black", lw=mylw, label="Mass") #Mass 
-    # ax1[:plot]([sol.t[1], sol.t[end]], [-1, -1], color="black", lw=mylw, label="Mass") #Phantom Mass for lagend
-    if rad
-        ax1[:plot](sol.t, sol[8, :], "--", color=blue3, lw=mylw, label="Chk2", alpha=0.9) # Chk2
-        ax1[:plot](sol.t, sol[9, :]/35., "--", color=green3, lw=mylw, label="ATM", alpha =0.6) # ATM
-        ax1[:plot](sol.t, sol[10, :]/35., "--", color=blue2, lw=mylw, label="DSB", alpha =0.6) # DSB
-        #=
-        rad_start = sol.prob.f.params[41]
-        rad_dur = sol.prob.f.params[42]
-        rad_rate = sol.prob.f.params[43]
-        rad_peak = [rad_start, sol(rad_start+rad_dur)[9]/35.0] + [0.0,0.02]
-        str_rad = @sprintf("Radiation pulse\n\$D = %4.2f\$ Gy", rad_dur*rad_rate)
-        arrowprops = Dict("arrowstyle"=>"fancy", "fc"=>"gray", "ec"=>"none")
-        annotate(str_rad, xy=rad_peak, xytext=rad_peak+[0.0,0.2], ha="center", arrowprops=arrowprops)
-        =#
+    if rad!=nothing
+        # fig[:subplots_adjust](right=0.8)
+        ax3 = ax1[:twinx]()
+        ax3[:spines]["right"][:set_position](["outward", 50])
+        rad_start, rad_dur, rad_rate = rad
+        ax1[:plot](sol.t, sol[8, :], "-", color=blue3, lw=mylw, label="Chk2", alpha=0.9) # Chk2
+        # ax3[:plot](sol.t, sol[9, :].*sol[10, :], "-", color=green3, lw=mylw, label=L"ATM\cdot DSB", alpha =0.6) # ATM
+        ax3[:plot](sol.t, sol[10, :], "--", color=blue2, lw=mylw, label="DSB", alpha =0.6) # DSB
+        # str_Dose = @sprintf("\$D = %4.2f\$", TrapRule(map(DoseRate, t), t))
+        # rad_peak = [rad_start, rad_rate]
+        str_rad = @sprintf("\$D = %4.2f\$ Gy\n\$t=%4.2f\$ hr", rad_dur*rad_rate*60, rad_start)
+        # arrowprops = Dict("width"=>2.5, "headwidth"=>6, "shrink"=>0.05, "fc"=>"black", "ec"=>"none")#"arrowstyle"=>"fancy", 
+        bbox_props = Dict("boxstyle"=>"square,pad=0","fc"=>"white", "alpha"=>0.5, "ec"=>"none")
+        # ax1[:annotate](str_rad, xy=rad_peak, xytext=rad_peak, ha="left", va="bottom", arrowprops=arrowprops, bbox=bbox_props)
+        ax3[:text](rad_start + maximum(sol.t)*0.01, sol.prob.p.k_d1*rad_rate, str_rad, ha="left", va="top", bbox=bbox_props)
+        ax3[:set_ylabel]("Number of DSB's", fontsize=14)
+        min = 0.0
+        max = round(maximum(sol[10,:]), 5)
+        factor = (max - min)/100*4
+        ax3[:set_ylim](min - factor, max + factor)
+        axs = [ax3, ax1, ax2]
+    else
+        axs = [ax1, ax2]
     end
-    ax1[:fill_between](sol.t, -1, -1, facecolor=colI, edgecolor="gray", alpha = 0.8, label="Interphase")
-    ax1[:fill_between](sol.t, -1, -1, facecolor=colM, edgecolor="gray", alpha = 0.8, label="M-phase")
-    handles, labels = unite_legends([ax2, ax1])
-    leg = ax2[:legend](handles, labels, loc="upper right", fontsize=12, labelspacing=0.3, borderpad=0.3)
-    leg[:get_frame]()[:set_alpha](0.98)
+    ax2[:fill_between](sol.t, -1, -1, facecolor=colI, edgecolor="gray", alpha = 0.8, label="Interphase")#Phantom interphase for legend 
+    ax2[:fill_between](sol.t, -1, -1, facecolor=colM, edgecolor="gray", alpha = 0.8, label="M-phase")#Phatom M-phase for legend
+    handles, labels = unite_legends(axs)
+    if rad!=nothing
+        leg = ax3[:legend](handles, labels, loc="upper right", fontsize=12, labelspacing=0.3, borderpad=0.3)
+    else
+        leg = ax2[:legend](handles, labels, loc="upper right", fontsize=12, labelspacing=0.3, borderpad=0.3)
+    end
+    leg[:get_frame]()[:set_alpha](0.95)
     ax1[:set_xlabel]("time (hr)", fontsize=14)
     ax1[:set_ylabel]("Concentration", fontsize=14)
     ax2[:set_ylabel]("Cell mass", fontsize=14)
-    # str_Dose = @sprintf("\$D = %4.2f\$", TrapRule(map(DoseRate, t), t))
-    # bbox_prop0 = Dict("boxstyle"=>"round", "fc"=>"white")
-    # text(180.0, 1.25, str_Dose, size=12, color="DarkSlateGray", bbox=bbox_prop0)
-    min = round(minimum(sol[1,:]), 1)
-    max = round(maximum(sol[1,:]), 1)
-    factor = (max - min)/100*4
     xlim(0.0, sol.t[end])
+    min = round(minimum(sol[[1,3],:]), 1)
+    max = round(maximum(sol[[1,3],:]), 1)
+    factor = (max - min)/100*4
     ax1[:set_ylim](min - factor, max + factor)
-    ax2[:set_ylim](0.0, sol.prob.p.K_Mass)
+    min = 0.0
+    max = sol.prob.p.K_Mass
+    factor = (max - min)/100*4
+    ax2[:set_ylim](min - factor, max + factor)
     
     # Cell cycle stages
-    divider = axgrid.make_axes_locatable(ax1)
+    divider = axgrid.make_axes_locatable(ax2)
     ax0 = divider[:new_vertical](size="4%")
     fig[:add_axes](ax0)
     xx = sol.t
@@ -82,8 +95,12 @@ function PlotSolution(sol, rad=false)
     ax0[:xaxis][:set_ticks_position]("none")
     ax0[:yaxis][:set_ticks_position]("none")
     axis([0., sol.t[end], 0., 1.])
-    divider = axgrid.make_axes_locatable(ax2)
+    divider = axgrid.make_axes_locatable(ax1)
     ax0 = divider[:new_vertical](size="4%")
+    if rad!=nothing
+        divider = axgrid.make_axes_locatable(ax3)
+        ax0 = divider[:new_vertical](size="4%")
+    end
 
     gcf()
 end # PlotSol
@@ -119,6 +136,7 @@ function PlotBifurcation(curve::BifurcationCurve, vars)
             end
         end
     end
+    gcf()
 end
 
 function unite_legends(axes)
